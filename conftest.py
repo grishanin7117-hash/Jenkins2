17f85463585cbd9d479fc6ae42dc78a7e26d4f47
Nikita Grishanin <grishanin7117@gmail.com>
2026-04-25 01:12:56 +0300

    добавил драйвер

--- conftest.py
+++ conftest.py
@@ -42,9 +42,10 @@ def setup_browser(request):
     print("PASSWORD:", password)
 
     driver = webdriver.Remote(
-        command_executor=f"http://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor="http://selenoid.autotests.cloud/wd/hub",
         options=options
     )
+
 
     browser = Browser(Config(driver=driver))
 
