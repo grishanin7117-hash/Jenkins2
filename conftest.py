797d3551a88678a001d30af6890d8514da3a7ac1
Nikita Grishanin <grishanin7117@gmail.com>
2026-04-25 02:38:19 +0300

    изменил конфтест

--- conftest.py
+++ conftest.py
@@ -1,56 +1,64 @@
 import os
-
+import base64
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
-from dotenv import load_dotenv
+from selenium.webdriver.remote.remote_connection import RemoteConnection
 
-from utils import attach
 
 DEFAULT_BROWSER_VERSION = "100.0"
 
 
 def pytest_addoption(parser):
     parser.addoption(
-        '--browser_version',
-        default='128.0'
+        "--browser_version",
+        default=None,
+        help="Browser version"
     )
-
-
-@pytest.fixture(scope='session', autouse=True)
-def load_env():
-    load_dotenv()
 
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
+    browser_version = browser_version if browser_version else DEFAULT_BROWSER_VERSION
 
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
+    options = Options()
+
+    options.set_capability("browserName", "chrome")
+    options.set_capability("browserVersion", browser_version)
+    options.set_capability("selenoid:options", {
+        "enableVNC": True,
+        "enableVideo": True
+    })
+
+    # 🔑 берём креды из Jenkins
+    login = os.getenv("LOGIN")
+    password = os.getenv("PASSWORD")
+
+    print("LOGIN:", login)
+    print("PASSWORD:", "***")
+
+    # 💥 фикс 401 через Basic Auth header
+    auth = base64.b64encode(f"{login}:{password}".encode()).decode()
+
+    executor = RemoteConnection(
+        "http://selenoid.autotests.cloud/wd/hub",
+        resolve_ip=False
+    )
+
+    executor.set_headers({
+        "Authorization": f"Basic {auth}"
+    })
 
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor=executor,
         options=options
     )
-    browser = Browser(Config(driver))
 
-    yield browser
+    # чтобы selene/selenium работал стабильно
+    driver.maximize_window()
 
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
-    browser.quit()
+    yield driver
+
+    driver.quit()
